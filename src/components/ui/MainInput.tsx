8f1b99a9b1c1cb392c16a8ed10adcdb769b6ef16
Santiago <santiagora1987@gmail.com>
2025-07-28 19:52:21 -0300

    configurations

---
+++ src/components/ui/MainInput.tsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+const MainInput = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    console.log('Component mounted or inputValue changed:', inputValue);
+  }, [inputValue]);
+
+  const handleOnChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    ev.preventDefault();
+    setInputValue(ev.target.value);
+  };
+  return <input type="text" onChange={handleOnChange} />;
+};
+
+export default MainInput;
