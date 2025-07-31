3b2b2f0977105c4dc843a23caa01084120c00f3c
Santiago <santiagora1987@gmail.com>
2025-07-30 21:38:58 -0300

    layout configurations

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,28 @@ import './App.css';
 
 function App() {
   return (
-    <main>
-      <h1>Sales</h1>
-    </main>
+    <>
+      <main>
+        <div className="main-input">
+          <h1>My App</h1>
+        </div>
+        <section className="sales-grid">
+          <h2>sales</h2>
+        </section>
+        <section className="payment">
+          <h2>Payment Section</h2>
+        </section>
+        <section className="side-menu">
+          <h2>Side Menu</h2>
+        </section>
+        <section className="bottom-menu">
+          <h2>Bottom Menu</h2>
+        </section>
+        <section className="number-buttons">
+          <h2>Number Buttons</h2>
+        </section>
+      </main>
+    </>
   );
 }
 
