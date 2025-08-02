631cf58ba342373bd1cb6365560a94932f9a0437
Santiago <santiagora1987@gmail.com>
2025-08-02 20:43:13 -0300

    adding new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import './App.css';
+import BottomMenu from './components/ui/BottomMenu';
+import MainInput from './components/ui/MainInput';
+import Payments from './components/ui/Payments';
+import SalesTable from './components/ui/SalesTable';
 
 function App() {
   return (
     <>
       <main>
-        <div className="main-input">
-          <h1>My App</h1>
-        </div>
+        <section className="main-input">
+          <MainInput textLabel="Receipt from Monday 31 Jun" />
+        </section>
         <section className="sales-grid">
-          <h2>sales</h2>
+          <SalesTable salesData={[]} />
+          <Payments />
         </section>
         <section className="payment">
           <h2>Payment Section</h2>
@@ -17,7 +22,7 @@ function App() {
           <h2>Side Menu</h2>
         </section>
         <section className="bottom-menu">
-          <h2>Bottom Menu</h2>
+          <BottomMenu />
         </section>
         <section className="number-buttons">
           <h2>Number Buttons</h2>
