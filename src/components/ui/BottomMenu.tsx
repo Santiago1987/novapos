30b931e0eb8ac41783a9a646608ddee9429edbd4
Santiago <santiagora1987@gmail.com>
2025-08-02 20:43:26 -0300

    btoom menu creation

---
+++ src/components/ui/BottomMenu.tsx
@@ -0,0 +1,116 @@
+const BottomMenu = () => {
+  return (
+    <div className="flex justify-start flex-wrap items-center p-2 rounded-md gap-0.5">
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-20 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+      <button
+        type="button"
+        className="bg-green-950 aspect-square text-center w-15 text-base hover:bg-blue-700 text-white font-bold border border-blue-700 rounded"
+      >
+        Print
+      </button>
+    </div>
+  );
+};
+
+export default BottomMenu;
