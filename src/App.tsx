1056709ad640c392f1a7af2267619c6542b95026
Santiago <santiagora1987@gmail.com>
2025-08-04 21:26:31 -0300

    Components finished

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import BottomMenu from './components/ui/BottomMenu';
 import MainInput from './components/ui/MainInput';
 import Payments from './components/ui/Payments';
 import SalesTable from './components/ui/SalesTable';
+import NumbersButtons from './components/ui/NumbersButtons';
+import SideMenu from './components/ui/SideMenu';
+import TotalsComponent from './components/ui/TotalsComponent';
+import Miselaneos from './components/common/Miselaneos';
 
 function App() {
   return (
@@ -15,17 +19,18 @@ function App() {
           <SalesTable salesData={[]} />
           <Payments />
         </section>
-        <section className="payment">
-          <h2>Payment Section</h2>
+        <section className="totals-grid">
+          <Miselaneos />
+          <TotalsComponent />
         </section>
         <section className="side-menu">
-          <h2>Side Menu</h2>
+          <SideMenu />
         </section>
         <section className="bottom-menu">
           <BottomMenu />
         </section>
         <section className="number-buttons">
-          <h2>Number Buttons</h2>
+          <NumbersButtons />
         </section>
       </main>
     </>
