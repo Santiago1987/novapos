7bbcdfd882004daf1ef424c6c3e513d4d01d0fd0
Santiago <santiagora1987@gmail.com>
2025-08-05 18:23:31 -0300

    bottom menu pages

--- src/components/ui/BottomMenu.tsx
+++ src/components/ui/BottomMenu.tsx
@@ -13,10 +13,41 @@ const items = [
   { button: 'Ten', isClicked: false },
   { button: 'Eleven', isClicked: false },
   { button: 'Twelve', isClicked: false },
+  { button: 'Twelve', isClicked: false },
+  { button: 'Thirteen', isClicked: false },
+  { button: 'Fourteen', isClicked: false },
+  { button: 'Fifteen', isClicked: false },
+  { button: 'Sixteen', isClicked: false },
+  { button: 'Seventeen', isClicked: false },
+  { button: 'Eighteen', isClicked: false },
+  { button: 'Nineteen', isClicked: false },
+  { button: 'Twenty', isClicked: false },
 ];
 
 const BottomMenu = () => {
   const [buttonsList, setButtons] = useState(items);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 12;
+  const totalPages = Math.ceil(items.length / itemsPerPage);
+
+  const currentItems = buttonsList.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const nextPage = () => {
+    console.log('Next page clicked ' + currentPage);
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const prevPage = () => {
+    console.log('prevPage page clicked ' + currentPage);
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
   const handleOnClick = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
@@ -40,22 +71,50 @@ const BottomMenu = () => {
   };
 
   return (
-    <div className="grid grid-cols-3 grid-rows-4 pl-1 pb-1 rounded-md gap-0.5 align-middle h-full">
-      {buttonsList.map((elem, index) => (
-        <button
-          type="button"
-          key={index}
-          id={`button-${index}`}
-          onClick={handleOnClick}
-          className={`text-white bg-gradient-to-r from-green-950 
+    <div className="flex flex-col w-full h-full">
+      <div className="grid grid-cols-3 grid-rows-4 pl-1 pb-1 rounded-md gap-0.5 align-middle h-96 w-full">
+        {currentItems.map((elem, index) => (
+          <button
+            type="button"
+            key={index}
+            id={`button-${index}`}
+            onClick={handleOnClick}
+            className={`text-white bg-gradient-to-r from-green-950 
           via-green-900 to-green-950 hover:bg-gradient-to-br 
           focus:scale-3d focus:outline-none dark:focus:ring-blue-800 
           font-medium rounded-md text-lg text-center transition-all duration-75
           ${elem.isClicked ? 'scale-110' : 'scale-100'}`}
+          >
+            {elem.button}
+          </button>
+        ))}
+      </div>
+      <div className="flex justify-center mt-1 mb-1 items-center gap-2">
+        <button
+          onClick={prevPage}
+          disabled={currentPage === 1}
+          className=" disabled:opacity-50 text-xs w-3 h-3 text-center"
         >
-          {elem.button}
+          {'<'}
         </button>
-      ))}
+
+        {Array.from({ length: totalPages }).map((_, index) => (
+          <span
+            key={index}
+            className={`w-2 h-2 rounded-full ${
+              index === currentPage - 1 ? 'bg-blue-500' : 'bg-gray-400'
+            }`}
+          />
+        ))}
+
+        <button
+          onClick={nextPage}
+          disabled={currentPage === totalPages}
+          className="disabled:opacity-50 text-xs w-3 h-3 text-center"
+        >
+          {'>'}
+        </button>
+      </div>
     </div>
   );
 };
