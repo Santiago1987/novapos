1af97f7500e32c1b5fbafdc183ef46c6354f101d
Santiago <santiagora1987@gmail.com>
2025-11-16 21:50:27 -0300

    environment varibles

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,34 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'node:path';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: './',
-  test: {
-    environment: 'happy-dom',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react(), tailwindcss()],
+    base: './',
+    test: {
+      environment: 'happy-dom',
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://pos-srvrpcg.dvlp.local:57772',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/sales': {
-        target: 'http://pos-srvrpcg.dvlp.local:57772',
-        changeOrigin: true,
-        secure: false,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
       },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_SERVER_URL_COLRYUT,
+          changeOrigin: true,
+          secure: false,
+        },
+        '/sales': {
+          target: env.VITE_SERVER_URL_COLRYUT,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
