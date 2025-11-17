7f40cd296919652433c041de1d6695ade381ff7d
santiago-sdp <santiago.ramos@sdp.biz>
2025-11-17 16:59:17 -0300

    font size

--- src/components/ui/customerView/CustomerPayments.tsx
+++ src/components/ui/customerView/CustomerPayments.tsx
@@ -14,9 +14,9 @@ const CustomerPayments = ({ payments }: Props) => {
       <div className="border w-full h-full bg-white rounded-lg overflow-y-auto">
         <table className="w-full border-collapse border-0 table-fixed">
           <thead>
-            <tr className="sticky top-0 z-10 h-15 font-bold text-3xl text-white bg-blue-950">
-              <th className="p-1 w-6/12">{t('payment_method', lang)}</th>
-              <th className="p-1 w-2/12 text-left">{t('quantity', lang)}</th>
+            <tr className="sticky top-0 z-10 h-15 font-bold text-2xl text-white bg-blue-950">
+              <th className="p-1 w-5/12">{t('payment_method', lang)}</th>
+              <th className="p-1 w-3/12">{t('quantity', lang)}</th>
               <th className="p-1 w-2/12">{t('amount', lang)}</th>
               <th className="p-1 w-2/12">{t('value', lang)}</th>
             </tr>
@@ -29,10 +29,10 @@ const CustomerPayments = ({ payments }: Props) => {
                     key={index}
                     className="border-b border-gray-200 text-2xl h-15 bg-white"
                   >
-                    <td className="p-1 text-start w-6/20 overflow-hidden text-ellipsis text-nowrap font-bold">
+                    <td className="p-1 text-start w-5/20 overflow-hidden text-ellipsis text-nowrap font-bold">
                       {pay.PayKind}
                     </td>
-                    <td className="p-1 text-end w-2/20">{1}</td>
+                    <td className="p-1 text-end w-3/20">{1}</td>
                     <td className="p-1 text-end w-2/20">{pay.PayAmt}</td>
                     <td className="p-1 text-end w-2/20">{pay.PayAmt}</td>
                   </tr>
