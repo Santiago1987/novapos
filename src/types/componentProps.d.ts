9461ddf51e55f4ac349fec21296a45c3ec707359
Santiago <santiagora1987@gmail.com>
2025-11-25 20:04:43 -0300

    removing non used files

--- src/types/componentProps.d.ts
+++
@@ -1,6 +0,0 @@
-export interface ButtonProps {
-  isSelected: boolean;
-  handleSelectComponent: (id: string, type: 'buttons' | 'tables') => void;
-  handleCopyComponent: (id: string, type: 'buttons' | 'tables') => void;
-  handleDeleteComponent: (id: string, type: 'buttons' | 'tables') => void;
-}
