19dcc7dc7c783d9a371b26f2244f7ffe10bbc765
Santiago <santiagora1987@gmail.com>
2025-12-07 15:06:19 -0300

    new components added

--- src/components/ui/editor/ComponentFactory.tsx
+++ src/components/ui/editor/ComponentFactory.tsx
@@ -1,7 +1,12 @@
 import { ComponentTypes } from '@/types/constTypes';
 import SalesTable from '@/components/ui/SalesTable';
 import PaymentsTable from '@/components/ui/PaymentsTable';
-import TotalsTable from '@/components/ui//TotalsTable';
+import TotalsComponent from '@/components/ui/TotalsComponent';
+import Logo from '@/components/ui/customerView/Logo';
+import LabelComponent from '@/components/ui/LabelComponent';
+import OperatorComponent from '../customerView/OperatorComponent';
+import CarouselImages from '../customerView/CarouselImages';
+import CarouselVideos from '../customerView/CarouselVideos';
 
 type Props = {
   type: keyof typeof ComponentTypes;
@@ -12,13 +17,15 @@ const ComponentFactory = ({ id, type }: Props) => {
   const componentDiccionary = {
     SALES_TABLE: <SalesTable id={id} />,
     PAYMENTS_TABLE: <PaymentsTable id={id} />,
-    TOTALS_TABLE: <TotalsTable id={id} />,
+    TOTALS_COMPONENT: <TotalsComponent id={id} />,
     PROMOTIONS_TABLE: <></>, // Placeholder for PromotionsTable component
     BUTTON: <></>, // Placeholder for Button component
-    LABEL: <></>, // Placeholder for Label component
+    LABEL: <LabelComponent id={id} />, // Placeholder for Label component
     INPUT: <></>, // Placeholder for Input component
-    CAROUSEL: <></>, // Placeholder for Carousel component
-    LOGO: <></>, // Placeholder for Logo component
+    CAROUSEL_IMAGES: <CarouselImages id={id} />,
+    CAROUSEL_VIDEOS: <CarouselVideos id={id} />,
+    LOGO: <Logo id={id} />,
+    OPEATOR: <OperatorComponent id={id} />,
     default: <></>,
   };
 
